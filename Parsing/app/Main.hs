f4977822d846e87fd3da30f10351a69439508f6c
Parker Hagmaier <parkerhagmaier@gmail.com>
2023-10-11 02:34:35 -0700

    bad parser

--- Parsing/app/Main.hs
+++ Parsing/app/Main.hs
@@ -9,7 +9,7 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
